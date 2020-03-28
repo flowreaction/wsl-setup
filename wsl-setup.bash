75441c4467f69079c1efc1bcb222609e812b7ea8
florian <florian.bopp90@gmail.com>
2020-03-28 23:05:10 +0100

    added personal changes

--- wsl-setup.bash
+++ wsl-setup.bash
@@ -1,39 +1,46 @@
 #!/bin/bash
-WIN_USERNAME=$1
+export WIN_USERNAME=$(powershell.exe '$env:UserName')
 
 # Get SSH keys from Windows environment 
 umask 077
 if [ ! -d ~/.ssh ]
 then
-  cp -r /mnt/c/Users/$WIN_USERNAME/.ssh ~/
+  cp -r /mnt/c/Users/flori/.ssh ~/
   chmod 600 ~/.ssh/config
   chmod 400 ~/.ssh/id*
 fi
 
 # Setup SSH agent, umask, aliases and prompt
-echo "\n#Setting less annoying permissions\numask 077\n" >> ~/.profile
-cat ./ssh-scripts >> ~/.profile
-cat ./aliases >> ~/.bash_aliases
-cat ./prompt.bash >> ~/.bashrc
+if [ -z "$WSL_SETUP_EXECUTED" ] #only concat once
+then
+  cat ./ssh-scripts >> ~/.profile
+  cat ./aliases >> ~/.bash_aliases
+  cat ./prompt.bash >> ~/.bashrc
+  echo export WSL_SETUP_EXECUTED=true >> ~/.bashrc
+fi
 
 # Import settings for git and vim
 cp vimrc ~/.vimrc
 cp gitconfig ~/.gitconfig
 
 ## Debian Specific stuff
-# Install ansible packages, for Debian like environments
+# Install packages, for Debian like environments
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install curl ansible vim openssh-client git man-db -y
+sudo apt install curl vim openssh-client git man-db -y
 # Repo clean up
 sudo apt autoremove --purge -y
 sudo apt clean all
+
 ## Debian specific stuff ends here
 
 # Let SSH not bother me everytime I connect to a new host, which happens a lot.
 sudo sed -i "s/#   StrictHostKeyChecking ask/    StrictHostKeyChecking no/g" /etc/ssh/ssh_config
+
 # Passwordless sudo
 echo "echo \"$USER ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers" | sudo bash
-# Delete this script
-rm -rf ~/wsl-setup
+
+# Delete this repo
+  ## rm -rf ~/wsl-setup
+
 exit 0
